03cab90178411a99065648ac1ceb6eab6de3b64c
Karan Chhabra <karanchhabra99@gmail.com>
2021-03-24 15:55:28 -0700

    Make Inverse Board

--- ChessEngine.py
+++ ChessEngine.py
@@ -39,7 +39,18 @@ class GameState():
         if self.Player_turn == 1:
             flag = self.human.play(self.board, start_square, end_square, self.Player_turn, self.last_move)
         else:
-            flag = self.human2.play(self.board, start_square, end_square, self.Player_turn, self.last_move)
+            if self.board[start_square[0], start_square[1]] == -1:
+                temp1 = self.board[start_square[0], start_square[1]]
+                self.board[start_square[0], start_square[1]] = 25
+                inverse_start = np.where(self.black_board == 25)
+                self.board[start_square[0], start_square[1]] = temp1
+
+                temp2 = self.board[end_square[0], end_square[1]]
+                self.board[end_square[0], end_square[1]] = 25
+                inverse_end = np.where(self.black_board == 25)
+                self.board[end_square[0], end_square[1]] = temp2
+
+                flag = self.human2.play(self.black_board, (inverse_start[0][0], inverse_start[1][0]), (inverse_end[0][0], inverse_end[1][0]), self.Player_turn, self.last_move)
 
         ## Makes the move
         if flag == 1:
@@ -66,33 +77,36 @@ class Move():
     def check_piece_and_play(self, board, current_location, next_location, Player_turn, last_move):
         self.Player_turn = Player_turn
         if abs(board[current_location[0], current_location[1]]) == 1:
-            if next_location in self.all_move_pawn(current_location, next_location):
-                check = self.is_possible_pawn(board, current_location, next_location, last_move)
-                if check[0]:
-                    flag = 1
-                    ##ToDo:
-                    if (current_location[0] == 7):
+            return self.pawn_move_checker_en_passant(board, current_location, next_location, Player_turn, last_move)
 
-                        new_piece = input("Q or R or N or B: ")
-                        if new_piece.lower() == "q":
-                            board[current_location[0], current_location[1]] = Player_turn * 9
-                        elif new_piece.lower() == "r":
-                            board[current_location[0], current_location[1]] = Player_turn * 5
-                        elif new_piece.lower() == "n":
-                            board[current_location[0], current_location[1]] = Player_turn * 3
-                        elif new_piece.lower() == "b":
-                            board[current_location[0], current_location[1]] = Player_turn * 2
-                        else:
-                            print("Invalid Input\nConverting the piece to Queen")
-                            board[current_location[0], current_location[1]] = Player_turn * 9
+    def pawn_move_checker_en_passant(self, board, current_location, next_location, Player_turn, last_move):
+        if next_location in self.all_move_pawn(current_location, next_location):
+            check = self.is_possible_pawn(board, current_location, next_location, last_move)
+            if check[0]:
+                flag = 1
+                ##ToDo:
+                if (current_location[0] == 7):
 
-                    ## En passant
-                    if check[1] == 1:
-                        if Player_turn == 1:
-                            board[current_location[0], next_location[1]] = 0
-                        else:
-                            board[current_location[0], next_location[1]] = 0
-                    return flag
+                    new_piece = input("Q or R or N or B: ")
+                    if new_piece.lower() == "q":
+                        board[current_location[0], current_location[1]] = Player_turn * 9
+                    elif new_piece.lower() == "r":
+                        board[current_location[0], current_location[1]] = Player_turn * 5
+                    elif new_piece.lower() == "n":
+                        board[current_location[0], current_location[1]] = Player_turn * 3
+                    elif new_piece.lower() == "b":
+                        board[current_location[0], current_location[1]] = Player_turn * 2
+                    else:
+                        print("Invalid Input\nConverting the piece to Queen")
+                        board[current_location[0], current_location[1]] = Player_turn * 9
+
+                ## En passant
+                if check[1] == 1:
+                    if Player_turn == 1:
+                        board[current_location[0], next_location[1]] = 0
+                    else:
+                        board[current_location[0], next_location[1]] = 0
+                return flag
         return 0
 
     def all_move_pawn(self, start_square, end_square):
